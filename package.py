fe07d530fcd82e9f939331a80367d2cf9caac4e7
Lanz, Jon (NBCUniversal) <jon.lanz@dreamworks.com>
2025-10-07 16:11:07 -0700

    MOONRAY-5773, MOONRAY-5752 VFX Reference Platform updates (#2193)
    * Rez-test fixes
    
    * Code review fixes
    
    * Code review fixes
    
    * Code review fixes

--- package.py
+++ package.py
@@ -29,17 +29,69 @@ help = ('For assistance, '
         "please contact the folio's owner at: moonbase-dev@dreamworks.com")
 
 variants = [
-    ['os-rocky-9',  'opt_level-optdebug', 'refplat-vfx2023.1', 'openimageio-2.4.8.0.x',  'gcc-11.x',       'openvdb-11', 'zlib-1.2.11.x'],
-    ['os-rocky-9',  'opt_level-debug',    'refplat-vfx2023.1', 'openimageio-2.4.8.0.x',  'gcc-11.x',       'openvdb-11', 'zlib-1.2.11.x'],
-    # openvdb-11 requires llvm-15 which conflicts with clang-17's llvm-17, so it stays at openvdb-10.
-    ['os-rocky-9',  'opt_level-optdebug', 'refplat-vfx2023.1', 'openimageio-2.4.8.0.x',  'clang-17.0.6.x', 'openvdb-10', 'zlib-1.2.11.x'],
-    ['os-rocky-9',  'opt_level-optdebug', 'refplat-vfx2024.0', 'openimageio-2.4.8.0.x',  'gcc-11.x',       'openvdb-11', 'zlib-1.2.11.x'],
-    ['os-rocky-9',  'opt_level-optdebug', 'refplat-vfx2025.0', 'openimageio-3.0',        'gcc-11.x',       'openvdb-12', 'zlib-1.2.11.x'],
-    ['os-rocky-9',  'opt_level-optdebug', 'refplat-vfx2022.0', 'openimageio-2.3.20.0.x', 'gcc-9.3.x.1',    'openvdb-9',  'zlib-1.2.11.x', 'opensubdiv-3.5.0.x.0'],
+    [   # variant 0
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2023.1',
+        'openimageio-2.4.8.0.x',
+        'gcc-11.x',
+        'openvdb-11',
+        'zlib-1.2.11.x'
+    ],
+    [   # variant 1
+        'os-rocky-9',
+        'opt_level-debug',
+        'refplat-vfx2023.1',
+        'openimageio-2.4.8.0.x',
+        'gcc-11.x',
+        'openvdb-11',
+        'zlib-1.2.11.x'
+    ],
+    [   # variant 2
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2023.1',
+        'openimageio-2.4.8.0.x',
+        'clang-17.0.6.x',
+        'openvdb-10', # openvdb-11 requires llvm-15 which conflicts with clang-17's llvm-17, so it stays at openvdb-10.
+        'zlib-1.2.11.x'
+    ],
+    [   # variant 3
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2024.0',
+        'openimageio-2.4.8.0.x',
+        'gcc-11.x',
+        'openvdb-11',
+        'zlib-1.2.11.x'
+    ],
+    [   # variant 4
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2025.0',
+        'openimageio-3.0',
+        'gcc-11.x',
+        'openvdb-12',
+        'zlib-1.2.11.x'
+    ],
+    [   # variant 5
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2022.0',
+        'openimageio-2.3.20.0.x',
+        'gcc-9.3.x.1',
+        'openvdb-9',
+        'zlib-1.2.11.x',
+        'opensubdiv-3.5.0.x.0'
+    ],
 ]
 
 conf_rats_variants = variants[0:2]
 conf_CI_variants = variants
+
+# Add ephemeral package to each variant.
+for i, variant in enumerate(variants):
+    variant.insert(0, '.moonray_variant-%d' % i)
 
 requires = [
     'amorphous',
@@ -67,7 +119,13 @@ private_build_requires = [
 commandstr = lambda i: "cd build/"+os.path.join(*variants[i])+"; ctest -j $(nproc)"
 testentry = lambda i: ("variant%d" % i,
                        { "command": commandstr(i),
-                         "requires": ["cmake-3.23"] + variants[i] } )
+                        "requires": ["cmake"],
+                        "on_variants": {
+                            "type": "requires",
+                            "value": [".moonray_variant-%d" % i],
+                            },
+                        "run_on": "explicit",
+                        }, )
 testlist = [testentry(i) for i in range(len(variants))]
 tests = dict(testlist)
 
