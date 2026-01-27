af970419b50af92ee816bfe79c702757a4068e7d
206689199_nbcuni <Ashley.Lee@dreamworks.com>
2026-01-27 13:22:47 -0800

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '17.36'
+    _version = '17.37'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
